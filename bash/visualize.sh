dd53b36103413dbb3e8a9a3cee70468adb646b87
Edouard Rabasse <rabasse.edouard@gmail.com>
2025-05-07 09:27:30 -0700

    sweep

--- bash/visualize.sh
+++ bash/visualize.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --account=def-martin4
-#SBATCH --job-name=vrp-sweep
+#SBATCH --job-name=visualise
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=2
-#SBATCH --mem=64G
-#SBATCH --time=12:00:00
-#SBATCH --output=logs/wandb-agent-%A_%a.log
+#SBATCH --mem=8G
+#SBATCH --time=1:00:00
+#SBATCH --output=logs/visualize-%A_%a.log
 #SBATCH --export=ALL,WANDB_API_KEY
 
 module load python/3.11 scipy-stack/2023b opencv/4.10.0
@@ -16,12 +16,4 @@ source "$SLURM_TMPDIR/env/bin/activate"
 pip install --no-index --upgrade pip
 pip install --no-index -r "$SLURM_SUBMIT_DIR/requirements-clean.txt"
 
-# login W&B
-wandb login --relogin "$WANDB_API_KEY"
-
-# votre sweep ID
-# SWEEP_ID=polytechnique-rabasse/VRP/d6ovvlqc
-SWEEP_ID=polytechnique-rabasse/VRP/45kh03fq
-
-# chaque tâche Array lance un agent
-wandb agent "$SWEEP_ID"
+python visualize.py model.weight_path=checkpoints/vgg_8_30.pth model=vgg 
