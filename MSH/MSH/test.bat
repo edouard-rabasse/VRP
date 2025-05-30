99031abf82f89a5ac083bdf383e7420aec25b69e
edouard-rabasse <rabasse.edouard@gmail.com>
2025-05-30 14:54:34 -0400

    Update configuration files and batch script
    Modified MSH_NUM_ITERATIONS and MAX_WD_B2P values in configuration7.xml and configuration8.xml. Updated the test.bat script to use the correct parameters for execution.

--- MSH/MSH/test.bat
+++ MSH/MSH/test.bat
@@ -1,5 +1,7 @@
 cd MSH/MSH
-java -Xmx14000m "-Djava.library.path=C:\gurobi1201\win64\bin" -cp "bin;C:\gurobi1201\win64\lib\gurobi.jar" main.Main_refineSolutionFixedEdges ExperimentsAllSets.txt 13 configuration8.xml 1 8
+
+java -Xmx14000m "-Djava.library.path=C:\gurobi1201\win64\bin" -cp "bin;C:\gurobi1201\win64\lib\gurobi.jar" main.Main_refineSolutionFixedEdges ExperimentsAllSets.txt 17 configuration8.xml 1 8
+
 
 
 cd .. 
