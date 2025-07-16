59b6c0ac7e77cf0bc080500ba8b4754e730c32a8
Edouard Rabasse <rabasse.edouard@gmail.com>
2025-07-16 08:53:36 -0700

    minor changes

--- bash/sweep_multi_gpu.sh
+++ bash/sweep_multi_gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-martin4
 #SBATCH --job-name=vrp-sweep
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=64G
 #SBATCH --time=24:00:00
